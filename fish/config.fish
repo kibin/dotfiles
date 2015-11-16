c34f89ed9747edd710c73e2f10be4783fd82107e
kibin ᕕ( ᐛ )ᕗ <mr.kibin@gmail.com>
2015-11-16 13:38:46 +0300

    remove greeting and unix eol’s

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,6 @@
-source ~/.config/fish/aliases.fishset -g fish_prompt_pwd_dir_length 0set -x EDITOR nvim
+source ~/.config/fish/aliases.fish
+
+set -g fish_prompt_pwd_dir_length 0
+set -x EDITOR nvim
+set -e fish_greeting
+
