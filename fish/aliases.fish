5c1591176ab62173979ea7a12d46ab6163a5f9e6
Kibin <ohkibin@gmail.com>
2020-10-20 18:53:57 +0300

    eofs

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,1 +1,22 @@
-alias g="git status";alias brd="brew doctor";alias gst="git stash";alias gstp="git stash pop";alias gpr="git pull --rebase";alias gpsh="git push";alias gd="git diff";alias gl="git log";alias glp="git log -p";alias gf="git fetch";alias gcof="git checkout -f";alias gcm="git commit -m";alias gca="git commit -am";alias gbr="git branch";alias rld="source ~/.config/fish/config.fish";alias haskell="ghci";alias pserve="python -m SimpleHTTPServer 8000";
+alias g="git status";
+
+alias brd="brew doctor";
+
+alias gst="git stash";
+alias gstp="git stash pop";
+alias gpr="git pull --rebase";
+alias gpsh="git push";
+alias gd="git diff";
+alias gl="git log";
+alias glp="git log -p";
+alias gf="git fetch";
+alias gcof="git checkout -f";
+alias gcm="git commit -m";
+alias gca="git commit -am";
+alias gbr="git branch";
+
+alias rld="source ~/.config/fish/config.fish";
+
+alias haskell="ghci";
+
+alias pserve="python -m SimpleHTTPServer 8000";
