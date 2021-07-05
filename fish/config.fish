10a897dd82482de3015b0b30c3faa8bf666b289f
Alice Kibin <ohkibin@gmail.com>
2021-07-05 15:39:17 +0300

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,11 @@
+# aliases
 source ~/.config/fish/aliases.fish
 
+# rbenv
+set -x PATH $HOME/.rbenv/bin $PATH; rbenv init - | source
+
 set -g fish_prompt_pwd_dir_length 0
+
 set -x EDITOR nvim
 set -e fish_greeting
-
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
